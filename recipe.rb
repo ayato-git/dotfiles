699d9e575da53573a9a7f3b85ab7c52e66c06419
ayato-git <ayato-git@users.noreply.github.com>
2023-04-23 16:08:18 +0900

    setup Homebrew with Brewfile

--- recipe.rb
+++ recipe.rb
@@ -6,9 +6,11 @@ dotfiles = {
   "files/gitconfig" => ".gitconfig",
   "files/zshenv" => ".zshenv",
   "files/zshrc" => ".zshrc",
+  "files/Brewfile" => ".Brewfile",
   "config" => ".config"
 }
 
+# create symbolic link.
 dotfiles.each do |src, dst|
   src_path = File.join(dotfiles_dir, src)
   dst_path = File.join(home_dir, dst)
@@ -16,4 +18,22 @@ dotfiles.each do |src, dst|
   link dst_path do
     to src_path
   end
+end
+
+# setup Homebrew
+execute 'install homebrew' do
+  command '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+  not_if 'which brew'
+end
+
+execute 'update homebrew' do
+  command 'brew update'
+end
+
+execute 'upgrade outdated packages' do
+  command 'brew upgrade'
+end
+
+execute 'install packages from Brewfile' do
+  command "brew bundle --global"
 end
