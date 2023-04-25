2ede304dff36f460d04d05f4371c6d73a35fbda8
ayato-git <ayato-git@users.noreply.github.com>
2023-04-25 20:53:42 +0900

    simplify mitamae install path

--- install.sh
+++ install.sh
@@ -3,16 +3,7 @@ set -e
 
 # Determine the system architecture
 ARCH=$(uname -m)
-
-# Set the default installation directory
-if [[ "$ARCH" == "x86_64" ]]; then
-    # For x86_64 systems, use the standard /usr/local/bin directory
-    MITAMAE_PATH="/usr/local/bin/mitamae"
-else
-    # For Apple Silicon (aarch64) systems, use the /opt/homebrew/bin directory
-    # which is the default installation path for Homebrew on M1 Macs
-    MITAMAE_PATH="/opt/homebrew/bin/mitamae"
-fi
+MITAMAE_PATH="/usr/local/bin/mitamae"
 
 # Download mitamae for x86_64 or aarch64 based on the system architecture
 if [[ ! -f "$MITAMAE_PATH" ]]; then
