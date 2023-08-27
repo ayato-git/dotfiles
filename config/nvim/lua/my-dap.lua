75972dd185c47ce8770c3bfeb4d0eb21da8ce14f
ayato-git <ayato-git@users.noreply.github.com>
2023-08-28 03:13:45 +0900

    [add] nvim-dap and nvim-dap-ui. both works via NeoVim cmdline

---
+++ config/nvim/lua/my-dap.lua
@@ -0,0 +1,33 @@
+local dap = require('dap')
+local dapui = require('dapui')
+local php_debug = require('jetpack').get('vscode-php-debug').path
+
+dap.adapters.php = {
+  type = 'executable',
+  command = 'node',
+  args = { php_debug .. '/out/phpDebug.js' }
+}
+
+dap.configurations.php = {
+  {
+    type = 'php',
+    request = 'launch',
+    name = 'Listen for Xdebug',
+    port = 9000,
+    pathMappings = {
+      ["/app"] = "${workspaceFolder}"
+    }
+  }
+}
+
+dapui.setup()
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
