68e22a093b9e82ceb76126f828dad2a16ffd0a3e
ayato-git <ayato-git@users.noreply.github.com>
2024-02-28 16:46:58 +0900

    feat(NeoVim): 括弧から素早く抜けるjlキーマップ追加

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -51,6 +51,7 @@ vim.opt.undofile  = true -- アンドゥファイルを作成する
 vim.opt.clipboard:append  "unnamedplus" -- *と+のレジスタをOSのクリップボードとシンクロ
 vim.keymap.set('c', "<D-v>",  '<C-R>+',  { noremap = true }) -- コマンドモードでcommand-vで貼り付け
 vim.keymap.set('i', 'jj',     '<ESC>',          nil)         -- jjで挿入モードから抜ける
+vim.keymap.set('i', 'jl',     '<Right>',        nil)         -- jlで1文字右に移動 ) や ' の右側に行くのに便利
 vim.keymap.set('n', '<Tab>',  '<Cmd>bnext<CR>', nil)
 vim.keymap.set('n', '<S-Tab>','<Cmd>bprev<CR>', nil)
 
