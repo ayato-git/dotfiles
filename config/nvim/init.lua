3c811feef44dafa49ae07a8e755b192a965b0b89
ayato-git <ayato-git@users.noreply.github.com>
2024-03-18 07:10:36 +0900

    feat(NeoVim): Vimのレジスタとクリップボードの使い方を変更

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -48,8 +48,9 @@ vim.opt.backupdir = vim.fn.stdpath("state") .. "/backup~//"
 vim.opt.undofile  = true -- アンドゥファイルを作成する
 
 ---[[ 編集 ]]
-vim.opt.clipboard:append  "unnamedplus" -- *と+のレジスタをOSのクリップボードとシンクロ
 vim.keymap.set('c', "<D-v>",  '<C-R>+',  { noremap = true }) -- コマンドモードでcommand-vで貼り付け
+-- see https://blog.atusy.net/2024/03/11/vim-gy-as-gui-yank/ 
+vim.keymap.set('n', 'gy',  '"+y', nil)                       -- gyでクリップボード(+のレジスタ)にyankする
 vim.keymap.set('i', 'jj',     '<ESC>',          nil)         -- jjで挿入モードから抜ける
 vim.keymap.set('i', 'jl',     '<Right>',        nil)         -- jlで1文字右に移動 ) や ' の右側に行くのに便利
 vim.keymap.set('n', '<Tab>',  '<Cmd>bnext<CR>', nil)
