9b65218ab4035add19881807cf59fcb196945bc0
ayato-git <ayato-git@users.noreply.github.com>
2024-07-20 05:07:46 +0900

    refactor(NeoVim): lspによる定義ジャンプをTelescope経由で実施して、複数候補の場合に対応

--- config/nvim/lua/my-lsp.lua
+++ config/nvim/lua/my-lsp.lua
@@ -35,42 +35,6 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
--- Go-to definition in a split window
-local function goto_definition(split_cmd)
-  local util = vim.lsp.util
-  local log = require("vim.lsp.log")
-  local api = vim.api
-
-  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
-  local handler = function(_, result, ctx)
-    if result == nil or vim.tbl_isempty(result) then
-      local _ = log.info() and log.info(ctx.method, "No location found")
-      return nil
-    end
-
-    if split_cmd then
-      vim.cmd(split_cmd)
-    end
-
-    if vim.tbl_islist(result) then
-      util.jump_to_location(result[1])
-
-      if #result > 1 then
-        util.set_qflist(util.locations_to_items(result))
-        api.nvim_command("copen")
-        api.nvim_command("wincmd p")
-      end
-    else
-      util.jump_to_location(result)
-    end
-  end
-
-  return handler
-end
-
--- Go-to definition in a rightbelow vertical split window
-vim.lsp.handlers["textDocument/definition"] = goto_definition('rightbelow vsplit')
-
 for _, lsp in ipairs(servers) do
   if lsp == 'intelephense' then
     -- see https://github.com/bmewburn/vscode-intelephense/blob/v1.9.5/package.json
